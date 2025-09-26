d9946f1b97a10fd1594443e6697edcb1a414be5b
kevxcyj <kevcyjunior@gmail.com>
2025-09-25 22:54:08 -0500

    fixed test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { server } from './mocks/server';
+import { afterAll, afterEach, beforeAll } from 'vitest';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
