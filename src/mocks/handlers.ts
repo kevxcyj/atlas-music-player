d39e0cf1ec8035d38a41481705a9e748621e5831
kevxcyj <kevcyjunior@gmail.com>
2025-09-25 22:28:25 -0500

    install msw/ created mocks folder

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,25 @@
+// src/mocks/handlers.ts
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  http.get('/api/v1/playlist', () => {
+    return HttpResponse.json([
+      {
+        id: 1,
+        title: 'Painted in Blue',
+        artist: 'Soul Canvas',
+        artwork: 'https://placehold.co/100x100',
+        duration: '5:55',
+        url: 'http://localhost:5173/api/v1/audio?songId=1',
+      },
+      {
+        id: 2,
+        title: 'Title Drift',
+        artist: 'Cosmic Seas',
+        artwork: 'https://placehold.co/100x100',
+        duration: '8:02',
+        url: 'http://localhost:5173/api/v1/audio?songId=2',
+      },
+    ]);
+  }),
+];
