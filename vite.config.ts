cad4cea9d752b72495d354820b07764934c5e267
kevxcyj <kevcyjunior@gmail.com>
2026-01-25 17:29:04 -0600

    fixed type error in main file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    include: ['src/__tests__/*.{test,spec}.{ts,tsx}'],
+  },
 });
